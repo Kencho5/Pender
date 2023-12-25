37d460bc1e71bed99e8d7fee566a87fdf0880a4e
Kencho5 <giokenchadze@gmail.com>
2023-12-25 16:12:35 +0400

    serve html file from rust actix

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,17 +1,24 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, HttpResponse, Result};
+use std::fs;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world")
+async fn index() -> Result<HttpResponse> {
+    // Read the contents of the HTML file
+    let html_content = fs::read_to_string("front/home.html")
+        .map_err(|e| {
+            println!("Error reading file: {}", e);
+            actix_web::error::ErrorInternalServerError("Failed to read file")
+        })?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(html_content))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .route("/", web::get().to(index))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
