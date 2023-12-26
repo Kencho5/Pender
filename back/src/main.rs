ff8b238c82ba0221700585064bf8efe692e42667
Kencho5 <giokenchadze@gmail.com>
2023-12-26 10:09:58 +0400

    final dir structure

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,24 +1,9 @@
-use actix_web::{web, App, HttpServer, HttpResponse, Result};
-use std::fs;
-
-async fn index() -> Result<HttpResponse> {
-    // Read the contents of the HTML file
-    let html_content = fs::read_to_string("front/home.html")
-        .map_err(|e| {
-            println!("Error reading file: {}", e);
-            actix_web::error::ErrorInternalServerError("Failed to read file")
-        })?;
-
-    Ok(HttpResponse::Ok().content_type("text/html").body(html_content))
-}
+use actix_web::{web, App, HttpResponse, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
