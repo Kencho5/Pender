8e9715e2012e87860162d9158ffe28934501ce64
Kencho5 <giokenchadze@gmail.com>
2023-12-27 17:06:20 +0400

    added css serving

--- back/src/main.rs
+++ back/src/main.rs
@@ -6,6 +6,8 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
     app.at("/assets").serve_dir("../front/assets/")?;
+    app.at("/static").serve_dir("../front/static/")?;
+
     app.at("/").serve_file("../front/home.html")?;
     app.at("/api/navbar").serve_file("../front/navbar.html")?;
 
