2aa0af9bbc3886e25ded6bd2a0522f554edf4857
Kencho5 <giokenchadze@gmail.com>
2023-12-28 10:52:38 +0400

    switched to rust tide + added templating

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,20 +1,28 @@
-#[macro_use]
-extern crate rocket;
-use rocket::fs::FileServer;
-use rocket_dyn_templates::Template;
+use tide::Request;
+use tide::Response;
+use tera::Tera;
+use tide_tera::prelude::*;
 use std::fs;
 
-#[get("/")]
-fn index() -> Template {
-    let navbar = fs::read_to_string("../front/navbar.html.hbs").expect("Not Found");
-    let context = ("navbar", navbar);
-    Template::render("home", &context)
-}
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    tide::log::start();
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .mount("/static", FileServer::from("../front/static"))
-        .attach(Template::fairing())
+    let mut tera = Tera::new("./front/templates/**/*")?;
+    tera.autoescape_on(vec!["html"]);
+
+    let mut app = tide::with_state(tera);
+
+    app.at("/assets").serve_dir("./front/assets/")?;
+    app.at("/static").serve_dir("./front/static/")?;
+
+    app.at("/").get(|req: tide::Request<Tera>| async move {
+        let tera = req.state();
+        let navbar = fs::read_to_string("./front/templates/navbar.html").expect("Not Found");
+
+        tera.render_response("home.html", &context! { "navbar" => &navbar})
+    });
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }
