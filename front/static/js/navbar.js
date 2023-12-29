7970e991e7a3bef0e2e4410e4cecbd0209462693
Kencho5 <giokenchadze@gmail.com>
2023-12-29 11:24:27 +0400

    fixed navbar animation issue

---
+++ front/static/js/navbar.js
@@ -0,0 +1,39 @@
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function animateContextMenuLinks() {
+  const links = document.querySelectorAll(".context-link");
+
+  for (let index = 0; index < links.length; index++) {
+    await delay(50);
+    links[index].classList.add("show-link");
+  }
+}
+
+async function animateHideContextMenuLinks() {
+  const links = document.querySelectorAll(".context-link");
+  const reversedLinks = Array.from(links).reverse();
+
+  for (let index = 0; index < reversedLinks.length; index++) {
+    await delay(50);
+    reversedLinks[index].classList.remove("show-link");
+  }
+
+  await delay(200);
+  contextMenu.classList.toggle("show-context-menu");
+  contextMenu.classList.toggle("hidden");
+}
+
+document.getElementById("hamburgerMenu").addEventListener("click", () => {
+  const contextMenu = document.getElementById("contextMenu");
+
+  if (contextMenu.classList.contains("hidden")) {
+    contextMenu.classList.toggle("show-context-menu");
+    contextMenu.classList.toggle("hidden");
+
+    animateContextMenuLinks();
+  } else {
+    animateHideContextMenuLinks();
+  }
+});
