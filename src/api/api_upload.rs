6ec3221e4d34e3d70e3c20d9567df84c61f54f67
Kencho5 <53686164+Kencho5@users.noreply.github.com>
2024-01-16 15:53:57 +0400

    test

--- src/api/api_upload.rs
+++ src/api/api_upload.rs
@@ -1,7 +1,7 @@
 use tera::Tera;
 use tide::{Request, Response, StatusCode};
 
-pub async fn api_upload_handler(req: Request<Tera>) -> tide::Result {
+pub async fn api_upload_handler(_req: Request<Tera>) -> tide::Result {
     let response = Response::new(StatusCode::Ok);
 
     Ok(response)
