0329197fb99f76e18a90be0ffe7f7713c2a05f34
Kencho5 <giokenchadze@gmail.com>
2024-04-11 12:46:59 +0400

    added register to router and shortened post/get request handling

--- src/register_routes.rs
+++ src/register_routes.rs
@@ -4,8 +4,14 @@ use crate::utils::*;
 
 pub fn register_routes(app: &mut Server<AppState>) {
     app.at("/").get(home::home_handler);
-    app.at("/login").get(login::login_handler);
-    app.at("/login").post(login::login_post_handler);
+
+    app.at("/login")
+        .get(login::login_handler)
+        .post(login::login_post_handler);
+
+    app.at("/register")
+        .get(register::register_handler)
+        .post(register::register_post_handler);
 
     // API ROUTES
     app.at("/api/set_language/:lang")
