55a49c57ab189d66159d065a2bcdfeca02e0c2f7
Kencho5 <giokenchadze@gmail.com>
2024-04-11 12:45:27 +0400

    added register route

---
+++ src/routes/register.rs
@@ -0,0 +1,27 @@
+use crate::{imports::*, utils};
+
+pub async fn register_handler(req: Request<AppState>) -> tide::Result {
+    let context = utils::common::get_context(&req).await.unwrap();
+
+    let state = req.state();
+    let response = state.tera.render_response("register.html", &context)?;
+
+    Ok(response)
+}
+
+pub async fn register_post_handler(mut req: Request<AppState>) -> tide::Result {
+    // let user: auth_struct::LoginData = req.body_form().await?;
+    //
+    // let secret = std::env::var("TIDE_SECRET")?;
+    // let key: Hmac<Sha256> = Hmac::new_from_slice(secret.as_bytes())?;
+    //
+    // let mut claims = BTreeMap::new();
+    // claims.insert("email", user.email);
+    //
+    // let token = claims.sign_with_key(&key)?;
+    //
+    // let session = req.session_mut();
+    // session.insert("_jwt", token)?;
+    //
+    Ok(tide::Redirect::new("/").into())
+}
