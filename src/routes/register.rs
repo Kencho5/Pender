007c7dcd1bb22527da559bc0fa300c2faa04ccc7
Kencho5 <giokenchadze@gmail.com>
2024-04-15 09:53:09 +0400

    refactored register handling

--- src/routes/register.rs
+++ src/routes/register.rs
@@ -15,17 +15,9 @@ pub async fn register_post_handler(mut req: Request<AppState>) -> tide::Result {
         .body("<p class='success'>Account Created!</p>")
         .build();
 
-    let check_existing = sqlx::query("SELECT id FROM users WHERE email = $1")
-        .bind(&user.email)
-        .fetch_one(pg_conn.acquire().await?)
-        .await;
-
-    match check_existing {
-        Ok(_) => {
-            response.set_body("<p class='error'>Email address already in use</p>");
-            return Ok(response);
-        }
-        Err(_) => {}
+    if email_already_exists(&mut pg_conn, &user.email).await {
+        response.set_body("<p class='error'>Email address already in use</p>");
+        return Ok(response);
     }
 
     if user.password.is_empty() {
@@ -33,28 +25,42 @@ pub async fn register_post_handler(mut req: Request<AppState>) -> tide::Result {
         return Ok(response);
     }
 
-    let pass_hash = bcrypt::hash(user.password).unwrap();
     let user_id = Uuid::new_v4();
 
+    if !register_user(&mut pg_conn, user_id, &user).await {
+        response.set_body("<p class='error'>Failed to register user</p>");
+        return Ok(response);
+    }
+
+    Ok(response)
+}
+
+async fn email_already_exists(pg_conn: &mut sqlx::PgConnection, email: &str) -> bool {
+    sqlx::query("SELECT id FROM users WHERE email = $1")
+        .bind(email)
+        .fetch_optional(pg_conn)
+        .await
+        .expect("Email fetching error")
+        .is_some()
+}
+
+async fn register_user(
+    pg_conn: &mut sqlx::PgConnection,
+    user_id: Uuid,
+    user: &auth_struct::RegisterData,
+) -> bool {
+    let pass_hash = bcrypt::hash(&user.password).unwrap();
     let registration_result = sqlx::query(
         "INSERT INTO users(id, email, name, phone, city, password) VALUES($1, $2, $3, $4, $5, $6)",
     )
     .bind(user_id.to_string())
-    .bind(user.email)
-    .bind(user.name)
-    .bind(user.phone)
-    .bind(user.city)
-    .bind(pass_hash)
-    .execute(pg_conn.acquire().await?)
+    .bind(&user.email)
+    .bind(&user.name)
+    .bind(&user.phone)
+    .bind(&user.city)
+    .bind(&pass_hash)
+    .execute(pg_conn)
     .await;
 
-    match registration_result {
-        Ok(_) => {}
-        Err(_) => {
-            response.set_body("<p class='error'>Fill in the form</p>");
-            return Ok(response);
-        }
-    }
-
-    Ok(response)
+    registration_result.is_ok()
 }
