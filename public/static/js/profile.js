4e0ade2f19d7172e61cdc7f4236a6929b93841db
Kencho5 <giokenchadze@gmail.com>
2024-04-16 15:31:16 +0400

    give active class to clicked tab

--- public/static/js/profile.js
+++ public/static/js/profile.js
@@ -1,4 +1,17 @@
-function switchTab(toHide, toShow) {
-  document.getElementsByClassName(toHide)[0].style.display = "none";
-  document.getElementsByClassName(toShow)[0].style.display = "block";
+function switchTab(el) {
+  const profileInfo = document.getElementsByClassName("profile-info")[0];
+  const postsInfo = document.getElementsByClassName("posts-info")[0];
+
+  document.querySelector(".active-profile-tab").classList.remove(
+    "active-profile-tab",
+  );
+  el.classList.add("active-profile-tab");
+
+  if (el.classList.contains("profile-tab")) {
+    postsInfo.style.display = "none";
+    profileInfo.style.display = "block";
+  } else {
+    postsInfo.style.display = "block";
+    profileInfo.style.display = "none";
+  }
 }
