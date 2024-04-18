a0ac00a3169c1d2f8c904d2faead80e3c548ff1c
Kencho5 <giokenchadze@gmail.com>
2024-04-18 11:42:02 +0400

    separated cities input as component

--- public/static/js/auth.js
+++ public/static/js/auth.js
@@ -1,33 +1,3 @@
-async function showCities() {
-  while (!document.querySelector(".cities-div > *")) {
-    await delay(50);
-  }
-
-  const input = document.getElementsByName("city_dummy")[0];
-  document.getElementsByClassName("cities-dropdown")[0].style.display = "block";
-
-  input.style.borderRadius = "10px 10px 0px 0px";
-  input.style.outline = "none";
-}
-
-const citiesDiv = document.querySelector(".cities-div");
-if (citiesDiv) {
-  citiesDiv.addEventListener("click", function (event) {
-    if (event.target.tagName === "P") {
-      var cityName = event.target.textContent;
-      var cityValue = event.target.classList[0];
-      const cityInput = document.getElementsByName("city_dummy")[0];
-
-      cityInput.value = cityName;
-      document.getElementsByName("city")[0].value = cityValue;
-
-      cityInput.style.borderRadius = "10px";
-      document.getElementsByClassName("cities-dropdown")[0].style.display =
-        "none";
-    }
-  });
-}
-
 function validateForm(formName) {
   const formInputs = Array.from(
     document.forms[formName].querySelectorAll("input"),
