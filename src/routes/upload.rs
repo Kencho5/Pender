ced3942d6d6c55471a74bb457fc4e0c7f34af0aa
Kencho5 <giokenchadze@gmail.com>
2024-04-22 17:30:15 +0400

    added reset password form

--- src/routes/upload.rs
+++ src/routes/upload.rs
@@ -4,7 +4,9 @@ pub async fn upload_handler(req: Request<AppState>) -> tide::Result {
     let context = utils::common::get_context(&req).await?;
 
     let state = req.state();
-    let response = state.tera.render_response("upload.html", &context)?;
+    let response = state
+        .tera
+        .render_response("components/upload.html", &context)?;
 
     Ok(response)
 }
