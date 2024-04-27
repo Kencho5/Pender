6d171c1b9b96b5a4f31821b571ae08c162b250a0
Kencho5 <giokenchadze@gmail.com>
2024-04-27 11:40:08 +0400

    fetch post from db

--- src/routes/post.rs
+++ src/routes/post.rs
@@ -1,10 +1,38 @@
-use crate::{imports::*, utils};
+use crate::{
+    imports::*,
+    utils::{self, upload_struct},
+};
 
 pub async fn post_handler(req: Request<AppState>) -> tide::Result {
-    let context = utils::common::get_context(&req).await?;
+    let mut context = utils::common::get_context(&req).await?;
+
+    let post_id = req.param("post_id").unwrap().to_string();
+
+    match get_post(&req, post_id).await {
+        Ok(post) => {
+            context.insert("post", &post);
+        }
+        Err(_) => {
+            context.insert("post", "error");
+        }
+    }
 
     let state = req.state();
     let response = state.tera.render_response("post.html", &context)?;
 
     Ok(response)
+}
+
+pub async fn get_post(
+    req: &Request<AppState>,
+    post_id: String,
+) -> tide::Result<upload_struct::PostStruct> {
+    let mut pg_conn = req.sqlx_conn::<Postgres>().await;
+
+    let post = sqlx::query_as::<_, upload_struct::PostStruct>("SELECT * FROM posts WHERE id = $1")
+        .bind(post_id)
+        .fetch_one(pg_conn.acquire().await?)
+        .await?;
+
+    Ok(post)
 }
--- src/utils/upload_struct.rs
+++ src/utils/upload_struct.rs
@@ -11,3 +11,16 @@ pub struct UploadForm {
     pub phone: String,
     pub description: String,
 }
+
+#[derive(serde::Deserialize, serde::Serialize, sqlx::FromRow)]
+pub struct PostStruct {
+    pub animal: String,
+    pub breed: String,
+    pub post_type: String,
+    pub price: String,
+    pub age_years: String,
+    pub age_months: String,
+    pub gender: String,
+    pub phone: String,
+    pub description: String,
+}
