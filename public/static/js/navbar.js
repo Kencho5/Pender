be7a2a1d0eb5eb5c5c81562e6161e62529170181
Kencho5 <giokenchadze@gmail.com>
2024-05-03 09:51:40 +0400

    added animal dropdown to search

--- public/static/js/navbar.js
+++ public/static/js/navbar.js
@@ -41,6 +41,30 @@ document.getElementById("hamburgerMenu").addEventListener("click", async () => {
   }
 });
 
+function showDropdown() {
+  const dropdownContent = document.querySelectorAll(".animal-dropdown-content");
+  dropdownContent.forEach(function (content) {
+    content.classList.toggle("active-animal-dropdown");
+  });
+}
+
+function selectAnimal(event) {
+  if (event.target.tagName === "P") {
+    const selectedClass = event.target.classList[0];
+    const selectedId = event.target.id;
+
+    document.querySelector(".animal-input").value = selectedId;
+    document.querySelector(".animal-dropdown > i").className =
+      `fa fa-${selectedClass}`;
+
+    showDropdown();
+  }
+}
+document.querySelector(".animal-dropdown-content").addEventListener(
+  "click",
+  selectAnimal,
+);
+
 async function reloadPage(ms) {
   await delay(ms);
   location.reload();
