72dac9a6c4c2e9afbf9b153055f9820d39f9fdca
Kencho5 <giokenchadze@gmail.com>
2024-05-23 12:06:11 +0400

    Added: toast notification on profile save

--- public/static/js/profile.js
+++
@@ -1,9 +0,0 @@
-var detailsDiv = document.querySelector(".details-div");
-
-function triggerGlow() {
-  detailsDiv.classList.add("saved");
-}
-
-detailsDiv.addEventListener("animationend", () => {
-  detailsDiv.classList.remove("saved");
-});
