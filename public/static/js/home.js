fe662fddece51f2ecae4a2ece56fee2da4f7366f
Kencho5 <giokenchadze@gmail.com>
2024-05-26 21:01:14 +0400

    Updated: made filters header fixed

--- public/static/js/home.js
+++ public/static/js/home.js
@@ -18,9 +18,9 @@ function selectChip(event) {
   const chip = event.target;
 
   if (!chip.classList.contains("input-div")) {
-    for (const chips of chip.parentNode.children) {
-      chips.classList.remove("active-chip");
-    }
+    // for (const chips of chip.parentNode.children) {
+    //   chips.classList.remove("active-chip");
+    // }
 
     chip.classList.toggle("active-chip");
   }
@@ -34,14 +34,42 @@ function selectAgeType(event) {
   const age = event.target;
 
   if (age.classList.length == 0) {
-    document.querySelector(".age-selector").childNodes[0].textContent =
-      ` ${age.textContent} `;
-    document.querySelector(".age-dropdown").classList.remove("show-age");
+    const ageSelector = document.querySelector(".age-selector").childNodes[0];
+    const ageDropdown = document.querySelector(".age-dropdown");
+
+    ageSelector.textContent = ` ${age.textContent} `;
+    ageSelector.parentNode.id = age.id;
+    ageDropdown.classList.remove("show-age");
   }
 }
 
 function saveFilters(form, event) {
   event.preventDefault();
+
+  const formData = new FormData(form);
+  const filters = {};
+
+  // Get the values of input fields
+  for (const [name, value] of formData.entries()) {
+    filters[name] = value;
+  }
+
+  // Get the values of selected chips
+  const chipInputs = form.querySelectorAll(".input-div[input-name]");
+  chipInputs.forEach((chipInput) => {
+    const inputName = chipInput.getAttribute("input-name");
+    const selectedChips = Array.from(
+      chipInput.querySelectorAll(".chip.active-chip"),
+    ).map((chip) => chip.id);
+    if (selectedChips.length > 0) {
+      filters[inputName] = selectedChips;
+    }
+  });
+
+  // Get age type
+  filters["ageType"] = document.querySelector(".age-selector").id;
+
+  console.log(filters);
 
   toggleFilters();
 }
