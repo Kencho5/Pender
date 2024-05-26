4abc840524f13549b0767dd8d8be4f8d190aec3f
Kencho5 <giokenchadze@gmail.com>
2024-05-26 10:56:14 +0400

    Added: chips to form parameters

--- public/static/js/home.js
+++ public/static/js/home.js
@@ -1,3 +1,5 @@
+// localStorage.removeItem("filters");
+
 function toggleFilters() {
   document.querySelector(".modal").classList.toggle("show-modal");
 }
@@ -28,12 +30,17 @@ function search(form) {
 
 function selectChip(event) {
   const chip = event.target;
+  const filters = JSON.parse(localStorage.getItem("filters")) || {};
 
   if (!chip.classList.contains("input-div")) {
-    for (const chips of event.target.parentNode.children) {
+    for (const chips of chip.parentNode.children) {
       chips.classList.remove("active-chip");
     }
 
+    // console.log(chip.parentNode.getAttribute("input-name"));
+    filters[chip.parentNode.getAttribute("input-name")] = chip.id;
     chip.classList.toggle("active-chip");
   }
+
+  localStorage.setItem("filters", JSON.stringify(filters));
 }
