5de4404e6be8e2f7fb7d8d156fe6fa3fc4fc1759
Kencho5 <giokenchadze@gmail.com>
2024-06-03 10:13:51 +0400

    Added: translation to home features

--- src/utils/generate_id.rs
+++ src/utils/generate_id.rs
@@ -2,6 +2,6 @@ use rand::Rng;
 
 pub fn generate_id() -> String {
     let mut rng = rand::thread_rng();
-    let id: u32 = rng.gen_range(100000..1000000);
+    let id: u32 = rng.gen_range(10000000..100000000);
     id.to_string()
 }
