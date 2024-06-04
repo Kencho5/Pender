0c8bdb635e6e4c04cd4b3a696b140d75ce92ca24
Kencho5 <giokenchadze@gmail.com>
2024-06-04 19:38:02 +0400

    Added: working arrow buttons

--- public/static/js/post.js
+++ public/static/js/post.js
@@ -1,12 +1,14 @@
 var currentImg = 0;
-function slideImage() {
+function changeImage(dir) {
   const images = document.querySelectorAll(".image");
 
-  if (currentImg >= images.length - 1) currentImg = 0;
+  if (dir == "left" && currentImg == 0) currentImg = images.length;
+  else if (dir == "right" && currentImg >= images.length - 1) currentImg = -1;
 
   for (const image of images) {
     if (image.classList.contains("active-image")) {
-      currentImg++;
+      if (dir == "left") currentImg--;
+      else currentImg++;
 
       const nextImg = document.querySelector(`[image-id="${currentImg}"]`);
       image.classList.remove("active-image");
