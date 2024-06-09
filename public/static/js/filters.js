89d8a9430b775bfc5c3595cea587c365b6d96166
Kencho5 <giokenchadze@gmail.com>
2024-06-09 17:37:34 +0400

    Added: basis search page actions

--- public/static/js/filters.js
+++ public/static/js/filters.js
@@ -92,3 +92,16 @@ filtersModal.addEventListener("click", function (event) {
     toggleFilters();
   }
 });
+
+function cleanFilters(filters) {
+  return Object.fromEntries(
+    Object.entries(filters).filter(([_, value]) => {
+      if (typeof value === "string") {
+        return value.trim() !== "";
+      } else if (Array.isArray(value)) {
+        return value.length > 0;
+      }
+      return true;
+    }),
+  );
+}
--- public/static/js/home.js
+++ public/static/js/home.js
@@ -7,16 +7,3 @@ function search(form) {
 
   return true;
 }
-
-function cleanFilters(filters) {
-  return Object.fromEntries(
-    Object.entries(filters).filter(([_, value]) => {
-      if (typeof value === "string") {
-        return value.trim() !== "";
-      } else if (Array.isArray(value)) {
-        return value.length > 0;
-      }
-      return true;
-    }),
-  );
-}
