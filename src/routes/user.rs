3c1871969fcae7c9e30f05ed60cb56201a90f177
Kencho5 <giokenchadze@gmail.com>
2024-06-12 23:05:22 +0400

    Added: user data retrieval

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -5,9 +5,17 @@ use crate::{
 
 pub async fn user_handler(mut req: Request<AppState>) -> tide::Result {
     let user_id = req.param("user_id").unwrap().to_string();
-    let user = get_user(&mut req, user_id).await?;
-
+    let mut user = get_user(&mut req, user_id).await?;
     let mut context = utils::common::get_context(&req).await?;
+    let session = req.session();
+    let lang = session.get::<String>("lang").unwrap_or("GEO".into());
+
+    let translations = utils::translations::load_translations("cities")
+        .await
+        .unwrap();
+    let city = &translations[lang].get(&user.city);
+
+    user.city = city.unwrap().as_str().unwrap().to_string();
     context.insert("user", &user);
 
     let state = req.state();
@@ -19,13 +27,13 @@ pub async fn user_handler(mut req: Request<AppState>) -> tide::Result {
 async fn get_user(
     req: &mut Request<AppState>,
     user_id: String,
-) -> tide::Result<Vec<auth_struct::UserPub>> {
+) -> tide::Result<auth_struct::UserPub> {
     let mut pg_conn = req.sqlx_conn::<Postgres>().await;
     let posts = sqlx::query_as::<_, auth_struct::UserPub>(
         "SELECT name, phone, city, facebook FROM users where id = $1",
     )
     .bind(user_id)
-    .fetch_all(pg_conn.acquire().await?)
+    .fetch_one(pg_conn.acquire().await?)
     .await?;
     Ok(posts)
 }
