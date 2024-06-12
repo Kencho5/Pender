cf5043dfc8ab5e24b49ccfa98a5375d6f9494e7e
Kencho5 <giokenchadze@gmail.com>
2024-06-12 10:44:24 +0400

    Fixed: path issue on analytics

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -7,16 +7,14 @@ pub async fn analytics_handler(req: Request<AppState>) -> tide::Result {
         .args(&[
             "/var/log/nginx/access.log",
             "-o",
-            "/tmp/report.html",
+            "/templates/report.html",
             "--log-format=COMBINED",
         ])
         .output()
         .expect("Failed to execute goaccess command");
 
     let state = req.state();
-    let response = state
-        .tera
-        .render_response("/tmp/report.html", &context! {})?;
+    let response = state.tera.render_response("report.html", &context! {})?;
 
     Ok(response)
 }
