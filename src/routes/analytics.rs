903d3ada9352023c006d51d8c636a2e073a31073
Kencho5 <giokenchadze@gmail.com>
2024-06-13 13:57:06 +0400

    Added: output for command

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -4,12 +4,13 @@ use std::process::Command;
 use tide::http::mime;
 
 pub async fn analytics_handler(_req: Request<AppState>) -> tide::Result {
-    Command::new("sudo")
+    let output = Command::new("sudo")
     .arg("sh")
     .arg("-c")
     .arg(r#"zcat /var/log/nginx/access.log*.gz ; cat /var/log/nginx/access.log* | goaccess -o report.html --log-format=COMBINED"#)
     .output()
     .expect("Failed to execute goaccess command");
+    println!("{:?}", output);
 
     let response = Response::builder(200)
         .body(read_to_string("./report.html")?)
