0c2209be56421f53d95c1171dca9ec8bba0440e1
Kencho5 <giokenchadze@gmail.com>
2024-06-13 14:52:55 +0400

    Updated: fonts route

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -1,14 +1,32 @@
 use crate::imports::*;
+use std::env;
 use std::fs::read_to_string;
+use std::path::Path;
 use std::process::Command;
 use tide::http::mime;
 
 pub async fn analytics_handler(_req: Request<AppState>) -> tide::Result {
-    let output = Command::new("bash")
-        .arg("report.sh")
+    let script_path = "./report.sh";
+
+    // Get the current directory path
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+
+    // Set the HOME environment variable to the current directory
+    env::set_var(
+        "HOME",
+        current_dir
+            .to_str()
+            .expect("Failed to convert current directory path"),
+    );
+
+    // Execute the script using the `sh` command
+    let output = Command::new("sh")
+        .arg(script_path)
         .output()
         .expect("Failed to execute script");
-    println!("{:?}", output);
+
+    // Print the output of the script if needed
+    println!("Script output: {}", String::from_utf8_lossy(&output.stdout));
 
     let response = Response::builder(200)
         .body(read_to_string("./report.html")?)
