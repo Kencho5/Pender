1eea4f4fb25d847540caa4986e72674facb1ea30
Kencho5 <giokenchadze@gmail.com>
2024-06-13 16:29:51 +0400

    Updated: analytics command

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -1,19 +1,18 @@
 use crate::imports::*;
-use std::env;
 use std::fs::read_to_string;
 use std::process::Command;
 use tide::http::mime;
 
 pub async fn analytics_handler(_req: Request<AppState>) -> tide::Result {
-    env::set_var("HOME", env::current_dir().unwrap());
-
-    let command = "zcat /var/log/nginx/access.log*.gz ; cat /var/log/nginx/access.log* | goaccess -o ./report.html --log-format=COMBINED";
-
-    let output = Command::new("sh")
+    let output = Command::new("sudo")
+        .arg("goaccess")
         .arg("-c")
-        .arg(command)
-        .spawn()
-        .expect("Failed to execute command");
+        .args(&[
+            r#"(zcat /var/log/nginx/access.log*.gz ; cat /var/log/nginx/access.log*) | goaccess -o report.html --log-format=COMBINED"#,
+        ])
+        .output()
+        .expect("Failed to execute goaccess command");
+
     println!("{:?}", output);
 
     let response = Response::builder(200)
