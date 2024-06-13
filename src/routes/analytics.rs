81ce931c5fb6cf96b77ff1c5356ac9c664bef5c7
Kencho5 <giokenchadze@gmail.com>
2024-06-13 15:56:26 +0400

    Updated: analytics command

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -1,16 +1,20 @@
 use crate::imports::*;
+use std::env;
 use std::fs::read_to_string;
 use std::process::Command;
 use tide::http::mime;
 
 pub async fn analytics_handler(_req: Request<AppState>) -> tide::Result {
+    env::set_var("HOME", env::current_dir().unwrap());
+
     let command = "zcat /var/log/nginx/access.log*.gz ; cat /var/log/nginx/access.log* | goaccess -o ./report.html --log-format=COMBINED";
 
-    Command::new("sh")
+    let output = Command::new("sh")
         .arg("-c")
         .arg(command)
         .spawn()
         .expect("Failed to execute command");
+    println!("{:?}", output);
 
     let response = Response::builder(200)
         .body(read_to_string("./report.html")?)
