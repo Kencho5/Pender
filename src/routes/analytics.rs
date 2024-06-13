0fdf07738a02f9471f62ee2ed832c5a0688d8713
Kencho5 <giokenchadze@gmail.com>
2024-06-13 12:57:41 +0400

    Added: font for windows

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -4,23 +4,18 @@ use std::process::Command;
 use tide::http::mime;
 
 pub async fn analytics_handler(_req: Request<AppState>) -> tide::Result {
-    let now = chrono::Utc::now();
-    let seven_days_ago = now - chrono::Duration::days(7);
-
     Command::new("sudo")
         .arg("goaccess")
         .args(&[
             "/var/log/nginx/access.log",
+            "--date-format=%Y-%m-%d",
+            "--date='2024-06-05'",
             "-o",
             "./report.html",
             "--log-format=COMBINED",
-            "--date-from=&",
-            &seven_days_ago.format("%Y-%m-%d").to_string(),
-            "--date-to=&",
-            &now.format("%Y-%m-%d").to_string(),
         ])
         .output()
-        .expect("Failed to execute command");
+        .expect("Failed to execute goaccess command");
 
     let response = Response::builder(200)
         .body(read_to_string("./report.html")?)
