fa8e02237ea40b91834c404a27c1d8e290f59deb
Kencho5 <giokenchadze@gmail.com>
2024-06-13 14:12:28 +0400

    Added: new font for testing

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -4,10 +4,11 @@ use std::process::Command;
 use tide::http::mime;
 
 pub async fn analytics_handler(_req: Request<AppState>) -> tide::Result {
-    Command::new("bash")
+    let output = Command::new("bash")
         .arg("./report.sh")
         .output()
         .expect("Failed to execute script");
+    println!("{:?}", output);
 
     let response = Response::builder(200)
         .body(read_to_string("./report.html")?)
