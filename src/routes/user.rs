da3c11bb1810d143dc74a1239bb92a7a392ec734
Kencho5 <giokenchadze@gmail.com>
2024-06-13 08:27:32 +0400

    Added: user posts count retrieval

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,12 +1,12 @@
 use crate::{
     imports::*,
-    utils::{self},
+    utils::{self, upload_struct},
 };
 
 pub async fn user_handler(mut req: Request<AppState>) -> tide::Result {
     let user_id = req.param("user_id").unwrap().to_string();
     let mut user = get_user(&mut req, &user_id).await?;
-    let posts = get_user_posts(&mut req, &user_id).await?;
+    let (posts, count) = get_user_posts(&mut req, &user_id).await?;
 
     let mut context = utils::common::get_context(&req).await?;
     let session = req.session();
@@ -20,6 +20,7 @@ pub async fn user_handler(mut req: Request<AppState>) -> tide::Result {
     user.city = city.unwrap().as_str().unwrap().to_string();
     context.insert("user", &user);
     context.insert("posts", &posts);
+    context.insert("count", &count);
 
     let state = req.state();
     let response = state.tera.render_response("user.html", &context)?;
@@ -44,11 +45,20 @@ async fn get_user(
 async fn get_user_posts(
     req: &mut Request<AppState>,
     user_id: &String,
-) -> tide::Result<auth_struct::UserPub> {
+) -> tide::Result<(Vec<upload_struct::PostStruct>, i64)> {
     let mut pg_conn = req.sqlx_conn::<Postgres>().await;
-    let posts = sqlx::query_as::<_, auth_struct::UserPub>("SELECT * FROM posts where user_id = $1")
+
+    let posts = sqlx::query_as::<_, upload_struct::PostStruct>(
+        "SELECT * FROM posts WHERE user_id = $1 LIMIT 16",
+    )
+    .bind(user_id)
+    .fetch_all(pg_conn.acquire().await?)
+    .await?;
+
+    let count: (i64,) = sqlx::query_as("SELECT COUNT(*) FROM posts WHERE user_id = $1")
         .bind(user_id)
         .fetch_one(pg_conn.acquire().await?)
         .await?;
-    Ok(posts)
+
+    Ok((posts, count.0))
 }
