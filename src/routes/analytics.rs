5ae6ff3ff0167e7a85e22e013a8536d2539f11c4
Kencho5 <giokenchadze@gmail.com>
2024-06-13 17:06:08 +0400

    Added: env path

--- src/routes/analytics.rs
+++ src/routes/analytics.rs
@@ -1,9 +1,14 @@
 use crate::imports::*;
+use std::env;
 use std::fs::read_to_string;
 use std::process::Command;
 use tide::http::mime;
 
 pub async fn analytics_handler(_req: Request<AppState>) -> tide::Result {
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let current_dir_str = current_dir.to_string_lossy();
+    env::set_var("HOME", &*current_dir_str);
+
     let output = Command::new("bash").arg("report.sh").output();
 
     println!("{:?}", output);
