d5b08ec2fc8479b69ddf68e63447b4d0065c2a01
Kencho5 <giokenchadze@gmail.com>
2024-06-14 17:16:25 +0400

    Fixed: analytics path

--- report.sh
+++ report.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-goaccess /var/log/nginx/access.log* -o report.html --log-format=COMBINED --load-from-disk
+goaccess /var/log/nginx/access.log -o report.html --log-format=COMBINED --load-from-disk
