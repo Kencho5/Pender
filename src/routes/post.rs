b275e2fdb2984f112b12f2079aca4a063bc53ef1
Kencho5 <giokenchadze@gmail.com>
2024-06-15 13:31:30 +0400

    Added: view count to posts

--- src/routes/post.rs
+++ src/routes/post.rs
@@ -2,11 +2,21 @@ use crate::{
     imports::*,
     utils::{self, upload_struct},
 };
+use sqlx::postgres::PgQueryResult;
 
-pub async fn post_handler(req: Request<AppState>) -> tide::Result {
+pub async fn post_handler(mut req: Request<AppState>) -> tide::Result {
     let mut context = utils::common::get_context(&req).await?;
 
     let post_id = req.param("post_id").unwrap().to_string();
+
+    let session = req.session_mut();
+    let mut viewed_posts = session.get::<String>("viewed_posts").unwrap_or_default();
+
+    if !viewed_posts.contains(&post_id) {
+        viewed_posts.push_str(&post_id);
+        session.insert("viewed_posts", viewed_posts)?;
+        increment_views(&mut req, &post_id).await?;
+    }
 
     match get_post(&req, post_id).await {
         Ok(post) => {
@@ -35,4 +45,16 @@ async fn get_post(
         .await?;
 
     Ok(post)
+}
+
+async fn increment_views(
+    req: &mut Request<AppState>,
+    post_id: &String,
+) -> Result<PgQueryResult, sqlx::Error> {
+    let mut pg_conn = req.sqlx_conn::<Postgres>().await;
+    let result = sqlx::query("UPDATE posts SET views = views + 1 WHERE id = $1")
+        .bind(post_id)
+        .execute(pg_conn.acquire().await?)
+        .await?;
+    Ok(result)
 }
