00917267c6c87c727f305c7838e2c397b10229bf
Kencho5 <giokenchadze@gmail.com>
2024-06-17 22:46:50 +0400

    Updated: primary color

--- scripts/sync_aws.sh
+++ scripts/sync_aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-aws s3 sync public/assets s3://pender-assets/assets/ --cache-control "max-age=3600"
-aws s3 sync public/static/css s3://pender-assets/css/ --cache-control "max-age=3600"
-aws s3 sync public/static/js s3://pender-assets/js/ --cache-control "max-age=3600"
+aws s3 sync public/assets s3://pender-assets/assets/ --cache-control "max-age=31536000"
+aws s3 sync public/static/css s3://pender-assets/css/ --cache-control "max-age=31536000"
+aws s3 sync public/static/js s3://pender-assets/js/ --cache-control "max-age=31536000"
 
 SSH_USER="$SSH_USERNAME"
 SSH_HOST="$SSH_HOSTNAME"
