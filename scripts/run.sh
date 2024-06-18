06a3dc74cf7a6543c97595adf0eec0a5ef00cb72
Kencho5 <giokenchadze@gmail.com>
2024-06-18 15:06:19 +0400

    Added: scss

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start Sass in watch mode without generating source maps
+sass --no-source-map --style compressed --watch public/static/scss:public/static/css --quiet &
+
+# Start cargo watch for Rust project
+cargo watch -x run --ignore scripts
+
