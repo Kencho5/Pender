d2e0bed09e386e3310a619eb11980ee8e1baaac8
Kencho5 <giokenchadze@gmail.com>
2024-06-19 09:46:22 +0400

    Updated: report script

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-goaccess /var/log/nginx/access.log /var/log/nginx/access.log.1 -o ../report.html --log-format=COMBINED
+goaccess /var/log/nginx/access.log /var/log/nginx/access.log.1 -o report.html --log-format=COMBINED
