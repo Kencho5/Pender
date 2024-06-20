b46e9e4ab4fbd6a905f1ecfbc869add89d9aba99
Kencho5 <giokenchadze@gmail.com>
2024-06-20 18:47:23 +0400

    Added: delete to aws s3

--- src/routes/delete.rs
+++ src/routes/delete.rs
@@ -1,6 +1,7 @@
 use crate::imports::*;
 use sqlx::postgres::PgQueryResult;
 use std::fs;
+use std::process::Command;
 
 pub async fn delete_handler(mut req: tide::Request<AppState>) -> tide::Result {
     let post_id = req.param("post_id").unwrap().to_string();
@@ -23,6 +24,13 @@ async fn delete_post(
         .await?;
 
     fs::remove_dir_all(format!("/var/uploads/post-images/{}", post_id))?;
+
+    Command::new("aws")
+        .arg("s3")
+        .arg("rm")
+        .arg(format!("s3://pender-assets/post-images/{}", post_id))
+        .output()
+        .expect("Failed to execute script");
 
     Ok(result)
 }
