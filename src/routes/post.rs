929a7e651f80d3d279689a1ab0abf73fb8005bc2
Kencho5 <giokenchadze@gmail.com>
2024-06-21 23:15:03 +0400

    Updated: post views to hashset

--- src/routes/post.rs
+++ src/routes/post.rs
@@ -1,24 +1,25 @@
-use crate::{
-    imports::*,
-    utils::{self, upload_struct},
-};
+use crate::imports::*;
+use crate::utils::common;
+use crate::utils::upload_struct;
 use sqlx::postgres::PgQueryResult;
+use std::collections::HashSet;
+use tide::{Request, Result};
 
-pub async fn post_handler(mut req: Request<AppState>) -> tide::Result {
-    let mut context = utils::common::get_context(&req).await?;
+pub async fn post_handler(mut req: Request<AppState>) -> Result {
+    let mut context = common::get_context(&req).await?;
 
     let post_id = req.param("post_id").unwrap().to_string();
 
     let session = req.session_mut();
-    let mut viewed_posts = session.get::<String>("viewed_posts").unwrap_or_default();
+    let mut viewed_posts: HashSet<String> =
+        session.get("viewed_posts").unwrap_or_else(HashSet::new);
 
-    if !viewed_posts.contains(&post_id) {
-        viewed_posts.push_str(&post_id);
+    if viewed_posts.insert(post_id.clone()) {
         session.insert("viewed_posts", viewed_posts)?;
         increment_views(&mut req, &post_id).await?;
     }
 
-    match get_post(&req, post_id).await {
+    match get_post(&req, &post_id).await {
         Ok(post) => {
             context.insert("post", &post);
         }
@@ -33,10 +34,7 @@ pub async fn post_handler(mut req: Request<AppState>) -> tide::Result {
     Ok(response)
 }
 
-async fn get_post(
-    req: &Request<AppState>,
-    post_id: String,
-) -> tide::Result<upload_struct::PostStruct> {
+async fn get_post(req: &Request<AppState>, post_id: &str) -> Result<upload_struct::PostStruct> {
     let mut pg_conn = req.sqlx_conn::<Postgres>().await;
 
     let post = sqlx::query_as::<_, upload_struct::PostStruct>("SELECT * FROM posts WHERE id = $1")
@@ -47,10 +45,7 @@ async fn get_post(
     Ok(post)
 }
 
-async fn increment_views(
-    req: &mut Request<AppState>,
-    post_id: &String,
-) -> Result<PgQueryResult, sqlx::Error> {
+async fn increment_views(req: &mut Request<AppState>, post_id: &str) -> Result<PgQueryResult> {
     let mut pg_conn = req.sqlx_conn::<Postgres>().await;
     let result = sqlx::query("UPDATE posts SET views = views + 1 WHERE id = $1")
         .bind(post_id)
