72432ced861554c1d82d0bbe6a3179d72f8fd6bd
Kencho5 <giokenchadze@gmail.com>
2024-06-26 13:26:24 +0400

    Added: total count of posts on view all

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -3,11 +3,21 @@ use crate::{
     utils::{self},
 };
 
-pub async fn home_handler(req: Request<AppState>) -> tide::Result {
-    let context = utils::common::get_context(&req).await?;
+pub async fn home_handler(mut req: Request<AppState>) -> tide::Result {
+    let mut context = utils::common::get_context(&req).await?;
+    context.insert("count", &get_posts_count(&mut req).await?);
 
     let state = req.state();
     let response = state.tera.render_response("home.html", &context)?;
 
     Ok(response)
+}
+
+async fn get_posts_count(req: &mut Request<AppState>) -> tide::Result<i64> {
+    let mut pg_conn = req.sqlx_conn::<Postgres>().await;
+    let count: (i64,) = sqlx::query_as("SELECT COUNT(*) FROM POSTS")
+        .fetch_one(pg_conn.acquire().await?)
+        .await?;
+
+    Ok(count.0)
 }
