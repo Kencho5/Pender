1a5f2ccf743d1687c84d849bb0204fca284f5ab8
Kencho5 <giokenchadze@gmail.com>
2024-06-27 10:05:40 +0400

    Updated: lazy load profile icons

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm /var/log/nginx/access.log.{6..10}.gz
+rm /var/log/nginx/access.log.7.gz
 goaccess <(cat /var/log/nginx/access.log /var/log/nginx/access.log.1) <(zcat /var/log/nginx/access.log.*.gz) -o report.html --log-format=COMBINED
