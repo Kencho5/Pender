2c3dff1fc7ff5138cbc50c9344637f461754b88e
Kencho5 <giokenchadze@gmail.com>
2024-07-08 16:39:40 +0400

    Fixed: hamburge menu icon url

--- scripts/sync_aws.sh
+++ scripts/sync_aws.sh
@@ -10,8 +10,8 @@ fi
 ENVIRONMENT=$1
 
 aws s3 sync public/assets s3://pender-assets/$ENVIRONMENT/assets/ --cache-control "max-age=31536000"
-aws s3 sync public/static/css s3://pender-assets/$ENVIRONMENT/css/ --cache-control "max-age=31536000"
-aws s3 sync public/static/js s3://pender-assets/$ENVIRONMENT/js/ --cache-control "max-age=31536000"
+aws s3 sync public/static/css s3://pender-assets/$ENVIRONMENT/static/css/ --cache-control "max-age=31536000"
+aws s3 sync public/static/js s3://pender-assets/$ENVIRONMENT/static/js/ --cache-control "max-age=31536000"
 
 SSH_USER="$SSH_USERNAME"
 SSH_HOST="$SSH_HOSTNAME"
